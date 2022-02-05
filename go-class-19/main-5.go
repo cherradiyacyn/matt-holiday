9af9fb7b7142b08b5c298e015ad7e6106aba5b46
Yacyn CHERRADI <cherradiyacyn@gmail.com>
2022-02-05 11:13:53 +0100

    first commit

--- go-class-19/main-5.go
+++ go-class-19/main-5.go
@@ -12,16 +12,25 @@ type Organ struct {
 
 type Organs []Organ
 
-func (s Organs) Len() int      { return len(s) }
-func (s Organs) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+func (s Organs) Len() int {
+	return len(s)
+}
 
-type ByName struct{ Organs }
+func (s Organs) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+type ByName struct {
+	Organs
+}
 
 func (s ByName) Less(i, j int) bool {
 	return s.Organs[i].Name < s.Organs[j].Name
 }
 
-type ByWeight struct{ Organs }
+type ByWeight struct {
+	Organs
+}
 
 func (s ByWeight) Less(i, j int) bool {
 	return s.Organs[i].Weight < s.Organs[j].Weight
