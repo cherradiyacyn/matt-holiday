864a5a93c88b847d1d99ef790ad895a31b962225
Yacyn CHERRADI <cherradiyacyn@gmail.com>
2023-02-02 15:15:02 +0100

    Networking with HTTP

---
+++ lesson_15/code_1/http-cl.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err := http.Get("http://localhost:3000/" + os.Args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(-1)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		fmt.Println(string(body))
+	}
+}
---
+++ lesson_15/code_2/code_2.1.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+const url = "https://jsonplaceholder.typicode.com"
+
+func main() {
+	resp, err := http.Get(url + "/todos/1")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(-1)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == http.StatusOK {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(-1)
+		}
+		fmt.Println(string(body))
+	}
+
+}
