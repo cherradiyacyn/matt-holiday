3636e2653255df3b8c89e98df31881445aa170e6
Yacyn CHERRADI <cherradiyacyn@gmail.com>
2023-02-02 15:14:02 +0100

    Reference & Value Semantics

---
+++ lesson_14/code_1.2.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	items := [][2]byte{{1, 2}, {3, 4}, {5, 6}}
+	a := [][]byte{}
+
+	for _, item := range items {
+		i := make([]byte, len(item))
+		copy(i, item[:])
+		a = append(a, i)
+	}
+
+	fmt.Println(items)
+	fmt.Println(a)
+}
